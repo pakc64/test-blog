fb9f31b9ea6ac1d869c428c4b4a938c5d4677847
pakc64 <djpakc@mail.ru>
2022-01-22 13:16:22 +0300

    blog test

---
+++ blog/src/main/java/com/pakcInc/blog/repo/PostRepository.java
@@ -0,0 +1,7 @@
+package com.pakcInc.blog.repo;
+
+import com.pakcInc.blog.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+}
